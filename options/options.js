bc2c5b434b5861738598bba4e2049d888c843ad9
Crowdbotics <team@crowdbotics.com>
2024-01-26 20:37:41 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "amerisave-mortgagem-45606",
+  url: "https://amerisave-mortgagem-45606.botics.co",
+  api: "https://amerisave-mortgagem-45606.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
